5751ab4b62867474986b10aa24152de30df2a928
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-01-10 21:10:18 +0200

    Backend: Remove unused `next` from default route

--- services/backend/src/routes/index.ts
+++ services/backend/src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.send({ success: true, message: 'Hello world!' });
 });
 
