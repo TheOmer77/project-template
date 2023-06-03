802935ff89b7d377925efdb6885879797ab34781
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-06-03 22:10:17 +0300

    Backend: Import & use something from template package

--- services/backend/src/routes/index.ts
+++ services/backend/src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { HELLO_WORLD } from '@theomer77/some-package';
 
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.send({ success: true, message: 'Hello world!' });
+  res.send({ success: true, message: HELLO_WORLD });
 });
 
 export default router;
