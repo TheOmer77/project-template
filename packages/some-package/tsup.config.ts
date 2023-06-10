09d13175f96428ad181c78c444c5c7052489bb08
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-06-10 14:01:15 +0300

    Remove `esbuild-plugin-eslint`

--- packages/some-package/tsup.config.ts
+++ packages/some-package/tsup.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'tsup';
 import { TsconfigPathsPlugin } from '@esbuild-plugins/tsconfig-paths';
-import eslint from 'esbuild-plugin-eslint';
 
 const config = defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
-  esbuildPlugins: [TsconfigPathsPlugin({}), eslint({ throwOnWarning: true })],
+  esbuildPlugins: [TsconfigPathsPlugin({})],
 });
 
 export default config;
