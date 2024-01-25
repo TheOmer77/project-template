764cd94540d20fb6b51c34704370decb467afd32
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-01-25 18:56:04 +0200

    Package: Remove `TsconfigPathsPlugin` from config

--- packages/some-package/tsup.config.ts
+++ packages/some-package/tsup.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'tsup';
-import { TsconfigPathsPlugin } from '@esbuild-plugins/tsconfig-paths';
 
 const config = defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   minify: true,
-  esbuildPlugins: [TsconfigPathsPlugin({})],
 });
 
 export default config;
