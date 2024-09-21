e92455b6022aaf7d0f1c9a6ad913d741d6b6eb3e
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-21 21:59:57 +0300

    Backend: Build d.ts in build script
    Build d.ts file when building for services other than backend itself,
    that use it as a dependency.

--- services/backend/build.ts
+++ services/backend/build.ts
@@ -1,10 +1,21 @@
-import { build } from 'esbuild';
+/// <reference types="node" />
 
-build({
-  entryPoints: ['src/index.ts'],
-  platform: 'node',
-  target: 'node20',
-  bundle: true,
-  minify: true,
-  outdir: 'dist',
-});
+import { build } from 'esbuild';
+import { build as buildDts } from 'tsup';
+
+const ENTRYPOINT = 'src/index.ts';
+
+Promise.all(
+  [
+    build({
+      entryPoints: [ENTRYPOINT],
+      platform: 'node',
+      target: 'node20',
+      bundle: true,
+      minify: true,
+      outdir: 'dist',
+    }),
+    process.env.APP_NAME !== 'backend' &&
+      buildDts({ entry: [ENTRYPOINT], dts: { only: true } }),
+  ].filter(Boolean)
+);
