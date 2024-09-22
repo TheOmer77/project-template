07f8a3b7811411199d5432cdc390f924fb44d76e
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-22 18:02:33 +0300

    Frontend: Switch to `@vitejs/plugin-react-swc`

--- services/frontend/vite.config.ts
+++ services/frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { checker } from 'vite-plugin-checker';
