33b3cdaf99fb4869b723e9c77915f2b669af2300
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-25 14:08:29 +0300

    Frontend: Add loading state

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,32 +4,36 @@ import { HELLO_WORLD } from '@repo/shared';
 
 import { apiClient } from '@/lib/api-client';
 
-const testBackend = async () => {
-  try {
-    const res = await apiClient.index.$get();
-    if (!res.ok)
-      throw new Error(
-        `Request failed${res.status ? ` with status code ${res.status}` : ''}`
-      );
-    const data = await res.json();
-    return JSON.stringify(data, undefined, 2);
-  } catch (err) {
-    return `Error connecting to backend:\n${
-      err instanceof Error ? err.message : err
-    }`;
-  }
-};
-
 const App = () => {
-  const [backendRes, setBackendRes] = useState<string>();
+  const [res, setRes] = useState<string>(),
+    [loading, setLoading] = useState(false);
+
+  const testBackend = async () => {
+    setLoading(true);
+    try {
+      const res = await apiClient.index.$get();
+      if (!res.ok)
+        throw new Error(
+          `Request failed${res.status ? ` with status code ${res.status}` : ''}`
+        );
+      const data = await res.json();
+      setRes(JSON.stringify(data, undefined, 2));
+    } catch (err) {
+      setRes(
+        `Error connecting to backend:\n${
+          err instanceof Error ? err.message : err
+        }`
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div>
       <h1>{HELLO_WORLD}</h1>
-      <button onClick={() => testBackend().then(setBackendRes)}>
-        Test connection to backend
-      </button>
-      {backendRes && <pre>{backendRes}</pre>}
+      <button onClick={testBackend}>Test connection to backend</button>
+      {(res || loading) && <pre>{loading ? 'Loading...' : res}</pre>}
     </div>
   );
 };
