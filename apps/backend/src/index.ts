731bfae9c6f86ae70cba05c548f8f2fbf9a04ece
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-25 13:59:02 +0300

    Backend: Separate app from index.ts

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,6 @@
 import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
 
-import { router } from '@/routes';
-
-const app = new Hono().route('/', router);
+import { app } from './app';
 
 serve(
   { fetch: app.fetch, port: Number(process.env.PORT) || 8000 },
