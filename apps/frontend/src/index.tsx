ab1aeba64b06d7b8d208a34dcb36fc9c0d227fc9
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-25 14:08:49 +0300

    Frontend: Reorganize imports in index.tsx

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
-
 import '@/styles/index.css';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+import App from './App';
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
