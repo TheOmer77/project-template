30a2f791d0dad0a1556d4b5a4808598491f2dfb8
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-25 14:03:55 +0300

    Backend: Don't build d.ts in build.ts script

--- apps/backend/build.ts
+++ apps/backend/build.ts
@@ -1,21 +1,10 @@
-/// <reference types="node" />
-
 import { build } from 'esbuild';
-import { build as buildDts } from 'tsup';
 
-const ENTRYPOINT = 'src/index.ts';
-
-Promise.all(
-  [
-    build({
-      entryPoints: [ENTRYPOINT],
-      platform: 'node',
-      target: 'node20',
-      bundle: true,
-      minify: true,
-      outdir: 'dist',
-    }),
-    process.env.APP_NAME !== 'backend' &&
-      buildDts({ entry: [ENTRYPOINT], dts: { only: true } }),
-  ].filter(Boolean)
-);
+build({
+  entryPoints: ['src/index.ts'],
+  platform: 'node',
+  target: 'node20',
+  bundle: true,
+  minify: true,
+  outdir: 'dist',
+});
