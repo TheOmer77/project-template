3cc9b47012c21affc827c26f6fd68facb782f4be
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-26 14:33:07 +0300

    Frontend: Change port to 3000 in both dev and prod
    This is for consistency and to allow the prod nginx service to properly
    serve the frontend. The actual nginx port in both dev and prod stays 80.

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
   ],
   server: {
     host: true,
+    port: 3000,
     hmr: { clientPort: 80, protocol: 'ws', host: 'localhost' },
   },
   cacheDir: '../../node_modules/.vite',
