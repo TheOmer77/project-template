75af89cb4c0a18f7697725022595b2db2df0a45c
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-09-27 15:58:23 +0300

    ESLint: Add some ESLint rules in base config

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -6,4 +6,25 @@ module.exports = {
     'prettier',
     'turbo',
   ],
+  plugins: ['check-file', 'prefer-arrow-functions'],
+  rules: {
+    'check-file/filename-naming-convention': [
+      'warn',
+      { '**/*.{ts,tsx}': 'KEBAB_CASE' },
+      { ignoreMiddleExtensions: true },
+    ],
+    'check-file/folder-naming-convention': ['warn', { 'src/**': 'KEBAB_CASE' }],
+    'prefer-arrow-callback': ['warn'],
+    'prefer-arrow-functions/prefer-arrow-functions': [
+      'warn',
+      {
+        allowNamedFunctions: false,
+        classPropertiesAllowed: false,
+        disallowPrototype: false,
+        returnStyle: 'implicit',
+        singleReturnOnly: false,
+      },
+    ],
+    'prefer-template': ['warn'],
+  },
 };
