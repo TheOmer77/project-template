b7cea85eb8052f4fe6aacf0e7c071e0425495460
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-10-11 15:51:49 +0300

    ESLint: Add import order rules

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -16,6 +16,58 @@ module.exports = {
       { ignoreMiddleExtensions: true },
     ],
     'check-file/folder-naming-convention': ['warn', { 'src/**': 'KEBAB_CASE' }],
+    'import-x/order': [
+      'warn',
+      {
+        alphabetize: { order: 'asc', orderImportKind: 'asc' },
+        distinctGroup: false,
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          ['parent', 'sibling', 'index'],
+          'type',
+        ],
+        'newlines-between': 'always',
+        pathGroups: [
+          { group: 'external', pattern: 'hono', position: 'before' },
+          { group: 'external', pattern: 'hono/**', position: 'before' },
+          { group: 'external', pattern: 'react', position: 'before' },
+          { group: 'external', pattern: 'react/**', position: 'before' },
+          { group: 'external', pattern: 'next', position: 'before' },
+          { group: 'external', pattern: 'next/**', position: 'before' },
+
+          { group: 'internal', pattern: '@repo/**', position: 'before' },
+          { group: 'internal', pattern: '@/components/ui/**' },
+          {
+            group: 'internal',
+            pattern: '@/components/**',
+            position: 'after',
+          },
+          { group: 'internal', pattern: '@/hooks/**', position: 'after' },
+          { group: 'internal', pattern: '@/routes/**', position: 'after' },
+          { group: 'internal', pattern: '@/utils/**', position: 'after' },
+          { group: 'internal', pattern: '@/lib/**', position: 'after' },
+          {
+            group: 'internal',
+            pattern: '@/config/**',
+            position: 'after',
+          },
+          {
+            group: 'internal',
+            pattern: '@/constants/**',
+            position: 'after',
+          },
+          {
+            group: 'internal',
+            pattern: '@/styles/**',
+            position: 'after',
+          },
+          { group: 'internal', pattern: '@/types/**', position: 'after' },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        warnOnUnassignedImports: true,
+      },
+    ],
     'prefer-arrow-callback': ['warn'],
     'prefer-arrow-functions/prefer-arrow-functions': [
       'warn',
