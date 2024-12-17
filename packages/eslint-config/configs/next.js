56402566b5f9cf733697e83ca1e5ab738a4391ba
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-12-17 15:00:24 +0200

    ESLint: Don't use `eslint-config-next` in next config
    Fix frontend lint errors by removing `eslint-config-next` and using
    react & next plugins directly. Some bits of this are copied from
    https://github.com/vercel/turborepo/blob/main/examples/basic/packages/eslint-config/next.js

--- packages/eslint-config/configs/next.js
+++ packages/eslint-config/configs/next.js
@@ -1,23 +1,48 @@
 import tailwind from 'eslint-plugin-tailwindcss';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReact from 'eslint-plugin-react';
+import pluginNext from '@next/eslint-plugin-next';
 
 import baseConfig from './base.js';
 import { nextCheckFile } from '../rules/check-file.js';
 import { nextRestrictedImports } from '../rules/restricted-imports.js';
-import compat from '../utils/compat.js';
 
 const nextConfig = tseslint.config(
   ...baseConfig,
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...pluginReact.configs.flat.recommended,
   ...tailwind.configs['flat/recommended'],
 
   nextCheckFile,
   nextRestrictedImports,
 
   {
+    plugins: { '@next/next': pluginNext },
+    rules: {
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs['core-web-vitals'].rules,
+    },
+  },
+
+  {
+    plugins: { 'react-hooks': pluginReactHooks },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      // React scope no longer necessary with new JSX transform.
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+
+  {
     languageOptions: {
-      globals: { ...globals.node, ...globals.browser },
+      ...pluginReact.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.serviceworker,
+      },
     },
   }
 );
